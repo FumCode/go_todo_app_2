135758af8aca702d3540c77b96e7a677e2cbeae2
FumCode <fumcode@gmail.com>
2025-01-10 11:20:43 +0200

    add health cheacker func

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type HealthResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	health := HealthResponse{
+		Status:  "ok",
+		Message: "Api is working",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(health)
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("App is running in port 3000")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error starting app", err)
+	}
 }
